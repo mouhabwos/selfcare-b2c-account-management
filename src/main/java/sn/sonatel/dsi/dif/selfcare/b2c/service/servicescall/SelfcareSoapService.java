46d9e22ab8406898f5618ac297cb89872037a47c
bouya <bouyakandee@outlook.com>
2019-04-01 17:54:59 +0200

    add test unit for service SelfcareSoapService.java

--- src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/service/servicescall/SelfcareSoapService.java
+++ src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/service/servicescall/SelfcareSoapService.java
@@ -2,10 +2,12 @@ package sn.sonatel.dsi.dif.selfcare.b2c.service.servicescall;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import sn.sonatel.dsi.dif.selfcare.b2c.config.Constants;
 import sn.sonatel.dsi.dif.selfcare.b2c.service.dto.AbonneDTO;
 import sn.sonatel.dsi.dif.selfcare.b2c.service.dto.SouscriptionDto;
 import sn.sonatel.dsi.dif.selfcare.b2c.web.rest.vm.SOAPRequest;
@@ -18,6 +20,7 @@ import java.util.List;
 @Service
 public class SelfcareSoapService {
 
+
     private final RestTemplate restTemplate;
 
     public SelfcareSoapService(@Qualifier("loadBalancedRestTemplate") RestTemplate restTemplate) {
@@ -26,24 +29,25 @@ public class SelfcareSoapService {
 
     public ResponseEntity<SouscriptionDto> getSouscription( HttpEntity<SOAPRequest> request){
 
-        ResponseEntity<SouscriptionDto> responseEntity = ServiceSelfcareb2cSOAP.getSouscription(restTemplate,request);
+        ResponseEntity<SouscriptionDto> responseEntity = restTemplate
+            .exchange(Constants.SELFCARE_B2C_SOAP_SERVICE+""+Constants.GET_SOUSCRIPTION_ABONNE, HttpMethod.POST, request, SouscriptionDto.class);
 
-        if(responseEntity.getStatusCode() == HttpStatus.OK) {
+        if(responseEntity != null && responseEntity.getStatusCode() == HttpStatus.OK) {
 
             return responseEntity;
 
         }else {
 
             return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
-
         }
 
     }
 
     public ResponseEntity<List<AbonneDTO>> getAbonne(HttpEntity<SOAPRequest> request){
 
-        ResponseEntity<List<AbonneDTO>> responseEntity = ServiceSelfcareb2cSOAP.getAbonne(restTemplate,request);
-        if(responseEntity.getStatusCode() == HttpStatus.OK){
+        ResponseEntity<List<AbonneDTO>> responseEntity = restTemplate
+            .exchange(Constants.SELFCARE_B2C_SOAP_SERVICE+""+Constants.GET_ABONNE, HttpMethod.POST, request,(Class) List.class);
+        if(responseEntity != null && responseEntity.getStatusCode() == HttpStatus.OK){
 
             return responseEntity;
 
@@ -52,5 +56,6 @@ public class SelfcareSoapService {
         }
 
     }
+
 
 }
--- src/test/java/sn/sonatel/dsi/dif/selfcare/b2c/service/servicescall/SelfcareSoapServiceTest.java
+++ src/test/java/sn/sonatel/dsi/dif/selfcare/b2c/service/servicescall/SelfcareSoapServiceTest.java
@@ -4,10 +4,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 import sn.sonatel.dsi.dif.selfcare.b2c.service.dto.AbonneDTO;
+import sn.sonatel.dsi.dif.selfcare.b2c.service.dto.SouscriptionDto;
 import sn.sonatel.dsi.dif.selfcare.b2c.web.rest.vm.SOAPRequest;
 
 import java.util.List;
@@ -17,18 +20,70 @@ import static org.mockito.Mockito.when;
 
 public class SelfcareSoapServiceTest {
 
-    @Mock
+
     private SelfcareSoapService soapService;
+
+    //@Autowired
+   // @Qualifier("loadBalancedRestTemplate")
+    @Mock
+    //@Autowired
+    private RestTemplate restTemplate;
+
 
 
     @Before
     public void setUp() throws Exception {
 
         MockitoAnnotations.initMocks(this);
+        soapService = new SelfcareSoapService(restTemplate);
+
     }
 
     @Test
     public void getSouscription() {
+        ResponseEntity<SouscriptionDto> response = ResponseEntity.status(HttpStatus.OK).build();
+
+        HttpEntity<SOAPRequest> request = new HttpEntity<>(new SOAPRequest("771326617"));
+        //Mockito.any()
+        when(soapService.getSouscription(request)).thenReturn(response);
+
+        ResponseEntity<SouscriptionDto> entity = soapService.getSouscription(request);
+
+        //System.out.println("===========<<<<<<>>>>>> "+entity.getStatusCode());
+
+        assertTrue(entity.equals(response));
+    }
+
+    @Test
+    public void getSouscriptionServiceUnavailable() {
+        ResponseEntity<SouscriptionDto> response = ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
+
+        HttpEntity<SOAPRequest> request = new HttpEntity<>(new SOAPRequest("771326617"));
+        //Mockito.any()
+        when(soapService.getSouscription(request)).thenReturn(response);
+
+        ResponseEntity<SouscriptionDto> entity = soapService.getSouscription(request);
+
+        assertTrue(entity.equals(response));
+    }
+
+
+
+    @Test
+    public void getAbonne() {
+        ResponseEntity<List<AbonneDTO>> response = ResponseEntity.status(HttpStatus.OK).build();
+
+        HttpEntity<SOAPRequest> request = new HttpEntity<>(new SOAPRequest("771326617"));
+        //Mockito.any()
+        when(soapService.getAbonne(request)).thenReturn(response);
+
+        ResponseEntity<List<AbonneDTO>> entity = soapService.getAbonne(request);
+
+        assertTrue(entity.equals(response));
+    }
+
+    @Test
+    public void getAbonneServiceUnavailable() {
         ResponseEntity<List<AbonneDTO>> response = ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
 
         HttpEntity<SOAPRequest> request = new HttpEntity<>(new SOAPRequest("771326617"));
@@ -37,10 +92,7 @@ public class SelfcareSoapServiceTest {
 
         ResponseEntity<List<AbonneDTO>> entity = soapService.getAbonne(request);
 
-        assertTrue(entity.getBody().isEmpty());
+        assertTrue(entity.equals(response));
     }
 
-    @Test
-    public void getAbonne() {
-    }
 }
