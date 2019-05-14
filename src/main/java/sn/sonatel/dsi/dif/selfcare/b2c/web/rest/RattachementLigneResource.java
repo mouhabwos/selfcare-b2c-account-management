d84b45a65115e7ac0ab554b27293abd708538654
Komi Serge Innocent <sergeKomi.innocent@orange-sonatel.com>
2019-05-14 19:45:16 +0000

    change authorization check to take place at the end of the call

--- src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/web/rest/RattachementLigneResource.java
+++ src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/web/rest/RattachementLigneResource.java
@@ -189,7 +189,7 @@ public class RattachementLigneResource {
     @Auditable(description = Message.Rattachement.List_By_MSISDN)
     @GetMapping("/rattachement-lignes/get-all-number/{msisdn}")
     @Timed
-    @PreAuthorize("#msisdn == authentication.name")
+    @PostAuthorize("#msisdn == authentication.name")
     public ResponseEntity<List<InfoNumberVM>> getRattachementLignes(
         @PathVariable String msisdn) {
         log.debug ( "REST request to get RattachementLigne : {}", msisdn );
@@ -224,6 +224,7 @@ public class RattachementLigneResource {
                 infoNumberVMList.add ( infoNumberVMS );
             }
         }
+
         return ResponseEntity.ok ( infoNumberVMList );
     }
 
