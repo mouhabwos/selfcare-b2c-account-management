aafe56dc3cca4968aeea02c308145ccdcb830073
Komi Serge Innocent <sergeKomi.innocent@orange-sonatel.com>
2019-05-16 09:39:30 +0000

    add check validity priority to send bad request if calling register without otp sent

--- src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/web/rest/AccountB2CResource.java
+++ src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/web/rest/AccountB2CResource.java
@@ -116,6 +116,11 @@ public class AccountB2CResource {
     @PostMapping("/register")
     public ResponseEntity<AccountB2C> registerAccountB2C(@Valid @RequestBody ManagedUserVM managedUserVM) throws URISyntaxException {
 
+
+        if (!otpService.checkRegisterValidity(managedUserVM.getLogin())){
+            return ResponseEntity.badRequest().build();
+        }
+
         Optional<AccountB2C> accountB2C = accountB2CRepository.findOneByNumero(managedUserVM.getLogin());
         Optional<RattachementLigne> ligne = rattachementLigneRepository.findByNumero(managedUserVM.getLogin());
 
@@ -126,10 +131,6 @@ public class AccountB2CResource {
         if(ligne.isPresent()){
             throw new LigneAlreadyRattachedException();
 
-        }
-
-        if (!otpService.checkRegisterValidity(managedUserVM.getLogin())){
-            return ResponseEntity.badRequest().build();
         }
 
 
