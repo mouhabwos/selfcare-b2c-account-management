ca4783ea01ad0fd0e5308c359b75f0e3cf1c6759
bouya <bouyakandee@outlook.com>
2019-05-23 16:54:16 +0200

    update pattern

--- src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/service/dto/NumeroDTO.java
+++ src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/service/dto/NumeroDTO.java
@@ -1,7 +1,7 @@
 package sn.sonatel.dsi.dif.selfcare.b2c.service.dto;
 
 import io.swagger.annotations.ApiModelProperty;
-import sn.sonatel.dsi.dif.selfcare.b2c.config.Constants;
+import sn.sonatel.dsi.dif.selfcare.b2c.web.rest.util.Constants;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
@@ -22,7 +22,7 @@ public class NumeroDTO {
     @ApiModelProperty(required = true)
     @NotNull(message = MessageValidation.NUMERO_NON_VIDE)
     @NotBlank(message = MessageValidation.NUMERO_NON_VIDE)
-   // @Pattern(regexp = Constants.LOGIN_REGEX_VALID_NUMBER, message = MessageValidation.NUMERO_ORANGE_VALIDE)
+    @Pattern(regexp = Constants.VALIDE_NUMBER_ORANGE_FIXE_MOBILE, message = MessageValidation.NUMERO_ORANGE_VALIDE)
     @Size(min = 9, max = 18, message = MessageValidation.NUMERO_TAILLE_VALIDE)
     @Column(name = "numero", nullable = false)
     protected String numero;
