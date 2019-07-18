9a5d23648ee2cc63efc9b0347c2e52947a313050
bouya <bouyakandee@outlook.com>
2019-07-18 13:09:38 +0200

    update file logging

--- src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/config/LogManagerConfiguration.java
+++ src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/config/LogManagerConfiguration.java
@@ -1,5 +1,6 @@
 package sn.sonatel.dsi.dif.selfcare.b2c.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import sn.sonatel.dsi.dif.selfcare.utils.selfcarelogging.interceptor.LogManager;
@@ -17,5 +18,5 @@ public class LogManagerConfiguration {
     }
 
     @Bean
-    public LogManager logManager() { return new LogManager(new FileLogger(),loggingProperties);}
+    public LogManager logManager(){return new LogManager(new FileLogger(),loggingProperties);}
 }
