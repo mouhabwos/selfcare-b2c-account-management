aa32c9d55c29e43e475773bb6ab8013a59cae961
Mouhamadou Bamba Mbacke SOW [SNT DSI/DAC/DIF/RAD] <MouhamadouBambaMbacke.SOW@orange-sonatel.com>
2019-07-18 09:26:22 +0000

    disable ssl verif

--- src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/SelfcareB2CApp.java
+++ src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/SelfcareB2CApp.java
@@ -113,4 +113,45 @@ public class SelfcareB2CApp {
         log.info("\n----------------------------------------------------------\n\t" +
                 "Config Server: \t{}\n----------------------------------------------------------", configServerStatus);
     }
+  
+    // -- disable Ssl Verification
+  
+    static {
+        disableSslVerification();
+    }
+
+    private static void disableSslVerification() {
+        try{
+            // Create a trust manager that does not validate certificate chains
+            TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }
+            };
+
+            // Install the all-trusting trust manager
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            // Create all-trusting host name verifier
+            HostnameVerifier allHostsValid = new HostnameVerifier() {
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            };
+
+            // Install the all-trusting host verifier
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+        } catch (NoSuchAlgorithmException e) {
+            log.error(e.getMessage(),e);
+        } catch (KeyManagementException e) {
+            log.error(e.getMessage(),e);
+        }
+    }
 }
