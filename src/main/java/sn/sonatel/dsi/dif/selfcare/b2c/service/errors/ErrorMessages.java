4ff666527d7987ffaedce3d74b7dd7b2d5a60fea
Bouya kande <bouyakande@pop-os.localdomain>
2019-11-11 17:33:25 +0000

    wording error

--- src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/service/errors/ErrorMessages.java
+++ src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/service/errors/ErrorMessages.java
@@ -2,7 +2,7 @@ package sn.sonatel.dsi.dif.selfcare.b2c.service.errors;
 
 public final class ErrorMessages {
 
-    public static final String NUMBER_IS_ALREADY_REGISTERED = "Ce numero est deja inscrit. Choisis un autre numero et essaie encore, bul xaadi, day baax !";
+    public static final String NUMBER_IS_ALREADY_REGISTERED = "Ce numero est deja parraine. Choisis un autre numero et essaie encore, bul xaadi, day baax !";
 
     public static final String NUMBER_ALREADY_SPONSORED = "Ce numero a deja ete selectionne. Choisis un autre numero et essaie encore, bul xaadi, day baax !";
 
