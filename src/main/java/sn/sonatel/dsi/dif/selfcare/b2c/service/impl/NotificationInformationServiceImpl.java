e7ae13e35575c3f21c2c73623a6eaa1f810af9af
Bouya kande <bouyakande@pop-os.localdomain>
2020-03-02 16:02:06 +0000

    SELFB2C-1788-account-enregistrer-la-formule-lors-de-la-creation-du-compte-async

--- src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/service/impl/NotificationInformationServiceImpl.java
+++ src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/service/impl/NotificationInformationServiceImpl.java
@@ -6,11 +6,12 @@ import org.springframework.stereotype.Service;
 import sn.sonatel.dsi.dif.selfcare.b2c.domain.NotificationInformation;
 import sn.sonatel.dsi.dif.selfcare.b2c.repository.NotificationInformationRepository;
 import sn.sonatel.dsi.dif.selfcare.b2c.service.NotificationInformationService;
+import sn.sonatel.dsi.dif.selfcare.b2c.service.client.apimanagement.CustomerOfferService;
+import sn.sonatel.dsi.dif.selfcare.b2c.service.client.model.CustomerOffer;
 import sn.sonatel.dsi.dif.selfcare.b2c.service.dto.NotificationInformationDTO;
 import sn.sonatel.dsi.dif.selfcare.b2c.service.mapper.NotificationInformationMapper;
 import sn.sonatel.dsi.dif.selfcare.b2c.web.rest.errors.LigneNotFoundException;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,10 +22,12 @@ public class NotificationInformationServiceImpl implements NotificationInformati
 
     private final NotificationInformationRepository notificationInformationRepository;
     private final NotificationInformationMapper notificationInformationMapper;
+    private final CustomerOfferService customerOfferService;
 
-    public NotificationInformationServiceImpl(NotificationInformationRepository notificationInformationRepository, NotificationInformationMapper notificationInformationMapper) {
+    public NotificationInformationServiceImpl(NotificationInformationRepository notificationInformationRepository, NotificationInformationMapper notificationInformationMapper, CustomerOfferService customerOfferService) {
         this.notificationInformationRepository = notificationInformationRepository;
         this.notificationInformationMapper = notificationInformationMapper;
+        this.customerOfferService = customerOfferService;
     }
 
 
@@ -51,5 +54,15 @@ public class NotificationInformationServiceImpl implements NotificationInformati
     public List<NotificationInformationDTO> getFirebaseIdByMsisdn(List<String> listMsisdn) {
         log.debug ( "SERVICE request to get FirebaseId By Msisdn with list msisdn");
         return notificationInformationMapper.toDto(notificationInformationRepository.findAllByAccountB2CNumeroIn(listMsisdn));
+    }
+
+    public void updateCodeFormuleCustomerOffer(String msisdn){
+        CustomerOffer customerOffer = customerOfferService.getCustomerOffer(msisdn);
+        NotificationInformationDTO informationDTO = new NotificationInformationDTO();
+        informationDTO.setMsisdn(msisdn);
+
+        informationDTO.setCodeFormule(customerOffer.getOfferId());
+
+
     }
 }
