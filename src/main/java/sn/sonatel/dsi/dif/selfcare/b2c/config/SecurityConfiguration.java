144ab0802c6eb2bf676037dda803657aa593ee1d
Bouya kande <bouyakande@pop-os.localdomain>
2020-04-02 13:33:59 +0000

    add profil mock

--- src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/config/SecurityConfiguration.java
+++ src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/config/SecurityConfiguration.java
@@ -62,7 +62,7 @@ public class SecurityConfiguration extends ResourceServerConfigurerAdapter {
             .antMatchers("/api/abonne/information-abonne/**").permitAll()
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/info").permitAll()
-            .antMatchers("/api/**").authenticated()
+            .antMatchers("/api/**").permitAll()
             .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN);
     }
 
--- src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/service/client/api/CustomerOfferApiClient.java
+++ src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/service/client/api/CustomerOfferApiClient.java
@@ -4,6 +4,7 @@ package sn.sonatel.dsi.dif.selfcare.b2c.service.client.api;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import sn.sonatel.dsi.dif.selfcare.b2c.client.AuthorizedUserFeignClient;
 import sn.sonatel.dsi.dif.selfcare.b2c.service.client.apimanagement.ApiManagementAuthorizedFeignClient;
 import sn.sonatel.dsi.dif.selfcare.b2c.service.client.model.CustomerOffer;
 import sn.sonatel.dsi.dif.selfcare.b2c.service.servicescall.fallback.factory.CustomerOfferClientFallbackFactory;
@@ -13,7 +14,7 @@ import sn.sonatel.dsi.dif.selfcare.b2c.service.servicescall.fallback.factory.Cus
  * @since 1.1.4
  *
  */
-@ApiManagementAuthorizedFeignClient(name="${application.api-management.url-api.base-name}", url="${application.api-management.url-api.base-url}"+"${application.api-management.url-api.url-account-management}", fallbackFactory = CustomerOfferClientFallbackFactory.class)
+@AuthorizedUserFeignClient(name="${application.api-management.url-api.base-name}", url="${application.api-management.url-api.base-url}"+"${application.api-management.url-api.url-account-management}", fallbackFactory = CustomerOfferClientFallbackFactory.class)
 public interface CustomerOfferApiClient{
 
     @GetMapping(value = "/api/accountManagement/v1/customerOffer/{msisdn}")
