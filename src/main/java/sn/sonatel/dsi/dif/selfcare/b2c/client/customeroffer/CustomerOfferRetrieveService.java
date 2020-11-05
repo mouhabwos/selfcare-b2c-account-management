0ae66c188b8abefda77734820ab4a7097e2efe96
Bamba Sow <mouhamadoubambambacke.sow@orange-sonatel.com>
2020-11-05 00:22:26 +0000

    fix cqache

--- src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/client/customeroffer/CustomerOfferRetrieveService.java
+++ src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/client/customeroffer/CustomerOfferRetrieveService.java
@@ -22,7 +22,7 @@ public class CustomerOfferRetrieveService {
     }
 
 
-    @Cacheable(value = API_MANAGEMENT_CUSTOMER_OFFER_NAME, key = "#msisdn", unless="#result == null |  #result.getBody() == null")
+    @Cacheable(value = API_MANAGEMENT_CUSTOMER_OFFER_NAME, key = "#msisdn", unless="#result.status!=200")
     public ResponseEntity getCachedCustomerOffer(String msisdn) {
 
         log.debug("Retrieving cached api management customer offer for user {} @@@@@@", msisdn);
