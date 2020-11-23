ca5c90433b70618b10446be13240cfe84b9b1beb
Sogui <sogui@pop-os.localdomain>
2020-11-23 12:44:30 +0000

    update token config

--- src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/config/ApplicationProperties.java
+++ src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/config/ApplicationProperties.java
@@ -253,7 +253,7 @@ public class ApplicationProperties {
 
         @Getter(AccessLevel.PUBLIC)
         @Setter(AccessLevel.PUBLIC)
-        private final Auth auth = new Auth();
+        private final Oauth2 oauth2 = new Oauth2();
 
         @Getter(AccessLevel.PUBLIC)
         @Setter(AccessLevel.PUBLIC)
@@ -263,7 +263,7 @@ public class ApplicationProperties {
         @Setter(AccessLevel.PUBLIC)
         private final PartyManagement partyManagement = new PartyManagement();
 
-        public static class Auth{
+        public static class Oauth2{
 
             @Setter(AccessLevel.PUBLIC)
             @Getter(AccessLevel.PUBLIC)
@@ -279,7 +279,7 @@ public class ApplicationProperties {
 
             @Setter(AccessLevel.PUBLIC)
             @Getter(AccessLevel.PUBLIC)
-            private String keyAccessTokenUri;
+            private String clientTokenUri;
 
         }
 
