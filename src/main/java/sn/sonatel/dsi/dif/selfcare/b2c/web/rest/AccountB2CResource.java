fe6c8531c7392549fbd33d3359817f5e735db29f
Bamba Sow <mouhamadoubambambacke.sow@orange-sonatel.com>
2021-01-29 10:06:33 +0000

    message register

--- src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/web/rest/AccountB2CResource.java
+++ src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/web/rest/AccountB2CResource.java
@@ -67,7 +67,7 @@ public class AccountB2CResource {
     }
 
 
-    //@Auditable(description = Message.Account.ADD)
+    @Auditable(description = Message.Account.ADD)
     @PostMapping("/register")
     public ResponseEntity<AccountB2C> registerAccountB2C(@Valid @RequestBody ManagedUserVM managedUserVM) {
         log.debug("REST request to save AccountB2C : {}", managedUserVM);
@@ -85,7 +85,7 @@ public class AccountB2CResource {
 
     }
 
-    //@Auditable(description = Message.Account.ADD)
+    @Auditable(description = Message.Account.ADD)
     @PostMapping("/v2/register")
     public ResponseEntity<AccountB2C> registerAccountB2CV2(@RequestHeader("X-UUID") String uuid, @Valid @RequestBody ManagedUserVM managedUserVM) {
         log.debug("REST request to save AccountB2C version 2 : {}", managedUserVM);
