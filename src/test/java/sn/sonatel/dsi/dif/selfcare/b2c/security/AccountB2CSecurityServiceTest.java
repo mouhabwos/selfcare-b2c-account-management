bece9b69a25158c5b02d04b59cb10a424e26703f
Komi Serge Innocent [GEBEYA DMGP/DD/DPD] <sergekomi.innocent@orange-sonatel.com>
2021-10-07 11:54:05 +0000

    SELFB2C-3734 [BACK-END] Exposer un endpoint pour faire la création implicite d'un compte fictif avec un mdp par défaut
    Modifier le call vers uaa en utilisant le loadbalancerresttemplate

--- src/test/java/sn/sonatel/dsi/dif/selfcare/b2c/security/AccountB2CSecurityServiceTest.java
+++ src/test/java/sn/sonatel/dsi/dif/selfcare/b2c/security/AccountB2CSecurityServiceTest.java
@@ -14,6 +14,7 @@ import org.springframework.security.oauth2.common.exceptions.InvalidClientExcept
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import sn.sonatel.dsi.dif.selfcare.b2c.domain.AccountB2C;
+import sn.sonatel.dsi.dif.selfcare.b2c.security.oauth2.OAuth2TokenEndpointClient;
 import sn.sonatel.dsi.dif.selfcare.b2c.service.AccountB2CService;
 import sn.sonatel.dsi.dif.selfcare.b2c.service.ValidationHmacService;
 import sn.sonatel.dsi.dif.selfcare.b2c.web.rest.vm.ManagedUserVM;
@@ -31,13 +32,15 @@ public class AccountB2CSecurityServiceTest {
     private  AccountB2CService accountB2CService;
     @Mock
     private ValidationHmacService validationHmacService;
+    @Mock
+    private OAuth2TokenEndpointClient authorizationClient;
 
     @Before
     public void setUp() {
         initMocks(this);
         jHipsterProperties= new JHipsterProperties();
         jHipsterProperties.getSecurity().getClientAuthorization().setAccessTokenUri("http://selfcare-uaa/oauth/token");
-        accountB2cSecurityService = new AccountB2CSecurityService(jHipsterProperties,restTemplate,accountB2CService, validationHmacService);
+        accountB2cSecurityService = new AccountB2CSecurityService(jHipsterProperties,restTemplate,accountB2CService, validationHmacService, authorizationClient);
     }
 
     @Test
