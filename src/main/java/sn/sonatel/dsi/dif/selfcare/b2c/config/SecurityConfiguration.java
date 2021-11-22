ae6a6957fb17c19b38d5d2c1531ce10d01b91fb4
Mouhamadou Bamba Sow <mouhamadoubambambacke.sow@orange-sonatel.com>
2021-11-22 09:50:11 +0000

    fix token propagation on async mode

--- src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/config/SecurityConfiguration.java
+++ src/main/java/sn/sonatel/dsi/dif/selfcare/b2c/config/SecurityConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
@@ -33,6 +34,8 @@ import java.util.Collections;
     private final OAuth2Properties oAuth2Properties;
 
     public SecurityConfiguration(OAuth2Properties oAuth2Properties) {
+
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
         this.oAuth2Properties = oAuth2Properties;
     }
 
